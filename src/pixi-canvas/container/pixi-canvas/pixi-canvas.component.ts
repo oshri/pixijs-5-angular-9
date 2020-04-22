d3960ba170346e4c83214a6f6f74aeddbd7e9397
Oshri <cms.x.seo@gmail.com>
2020-04-22 16:25:05 +0300

    Update config & add requestRender plus stop system and shared ticker

--- src/pixi-canvas/container/pixi-canvas/pixi-canvas.component.ts
+++ src/pixi-canvas/container/pixi-canvas/pixi-canvas.component.ts
@@ -10,7 +10,7 @@ import {
 	OnChanges,
 	SimpleChanges
 } from '@angular/core';
-import { Application, Graphics, Container } from 'pixi.js';
+import * as PIXI from 'pixi.js';
 import { Viewport } from 'pixi-viewport';
 import { NodesTypesService } from '../../nodeTypes/nodeTypes.service';
 
@@ -28,13 +28,14 @@ export class PixiCanvasComponent implements AfterViewInit, OnDestroy {
 	WORLD_HEIGHT: number;
 	canvasWidth: number;
 	canvasHeight: number;
-	linesContainer: Container;
+	linesContainer: PIXI.Container;
 
 	private defaults: IPixiApplicationOptions = {
 		transparent: true,
-		antialias: true,
+		antialias: false,
 		resolution: this.devicePixelRatio,
-		autoDensity: true
+		autoDensity: true,
+		autoStart: true
 	};
 
 	@Input() data: any;
@@ -42,7 +43,8 @@ export class PixiCanvasComponent implements AfterViewInit, OnDestroy {
 	@ViewChild('container', { read: ElementRef, static: true })
 	container: ElementRef;
 
-	pixiApp: Application = null;
+	pixiApp: PIXI.Application = null;
+	renderRequestId = undefined;
 
 	constructor(private ngZone: NgZone, private nodesTypesService: NodesTypesService) {
 		
@@ -59,7 +61,27 @@ export class PixiCanvasComponent implements AfterViewInit, OnDestroy {
 
 	private setStage(): void {
 		this.ngZone.runOutsideAngular(() => {
-			this.pixiApp = new Application(this.defaults);
+			this.pixiApp = new PIXI.Application(this.defaults);
+				
+			
+			// this.pixiApp.ticker.add(() => {
+			// 	console.log('app tick');
+			// 	this.pixiApp.ticker.stop();
+			// });
+			
+			
+
+			PIXI.Ticker.shared.add(() => {
+				console.log('shared tick');
+				PIXI.Ticker.shared.stop();
+			});
+
+
+			PIXI.Ticker.system.add(() => {
+				console.log('system tick');
+				PIXI.Ticker.system.stop();
+			});
+
 
 			this.WORLD_WIDTH = this.canvasWidth * 2;
 			this.WORLD_HEIGHT = this.canvasHeight * 2;
@@ -75,10 +97,26 @@ export class PixiCanvasComponent implements AfterViewInit, OnDestroy {
 			this.resize();
 			this.pixiApp.stage.addChild(this.viewport);
 
-			this.viewport.drag().pinch().wheel().decelerate();
-		});
+			this.viewport
+				.drag()
+				.pinch()
+				.wheel();
 
+			});
+			
+		this.requestRender();
 		this.container.nativeElement.appendChild(this.pixiApp.view);
+	}
+
+	private requestRender() {
+
+		if (this.renderRequestId) {
+			return;
+		}
+		this.renderRequestId = window.requestAnimationFrame(() => {
+			this.pixiApp.render();
+			this.renderRequestId = undefined;
+		});
 	}
 
 	private drawStage(): void {
@@ -89,13 +127,13 @@ export class PixiCanvasComponent implements AfterViewInit, OnDestroy {
 		/**
 		 * Draw Edges
 		 */
-		const lines = new Container();
+		const lines = new PIXI.Container();
 
 		this.data.edges.forEach((edge: IEdge) => {
 			const sourceCords: IEdgePosition = this.nodesTypesService.getNodeCords(this.data.location[edge.source]);
 			const targetCords: IEdgePosition = this.nodesTypesService.getNodeCords(this.data.location[edge.dest]);
 
-			const line: Graphics = this.createLine(edge, sourceCords, targetCords);
+			const line: PIXI.Graphics = this.createLine(edge, sourceCords, targetCords);
 			lines.addChild(line);
 		});
 
@@ -103,8 +141,8 @@ export class PixiCanvasComponent implements AfterViewInit, OnDestroy {
 		this.viewport.addChild(this.linesContainer);
 	}
 
-	private createLine(edge: IEdge, sourceCords: IEdgePosition, targetCords: IEdgePosition): Graphics {
-		const line = new Graphics();
+	private createLine(edge: IEdge, sourceCords: IEdgePosition, targetCords: IEdgePosition): PIXI.Graphics {
+		const line = new PIXI.Graphics();
 		// |
 		// |
 		//  \
